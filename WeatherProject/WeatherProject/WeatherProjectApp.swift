ccdfb7c4ef23f032f4db6c6c0a77dc96b110a763
ehdudgns <justin941@naver.com>
2024-04-09 00:38:04 +0900

    [추가] 프로젝트 추가

---
+++ WeatherProject/WeatherProject/WeatherProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherProjectApp.swift
+//  WeatherProject
+//
+//  Created by 도영훈 on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
